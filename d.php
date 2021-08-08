f145f647e13e26892a2856cba49c461ea81bc888
Param-Legend <79199733+Param-Legend@users.noreply.github.com>
2021-08-08 16:19:12 +0530

    Add files via upload

---
+++ d.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_Post['name'];
+$visitor_email = $_Post['email'];
+$message = $_Post['message'];
+
+$email_form = 'paramtogadiya@gmail.com';
+
+
+$email_subject = 'New Form Submission';
+
+
+$email_body = "User Name : $name.\n".
+
+"User Email : $visitor_email.\n".
+
+"User Message : $message.\n";
+
+
+
+$to = "paramtogadiya31@gmail.com";
+$headers = "Form : $email_form \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+header("Location : index.html ");
+
+?>
